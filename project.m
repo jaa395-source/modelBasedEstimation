4dc280adfd54147b34ec7994cc56e5057e9a42df
jaa395-source <jaa395@cornell.edu>
2026-04-25 17:27:09 -0400

    sure

--- project.m
+++ project.m
@@ -1,11 +1,15 @@
 %% Define Top Level Parameters
 hypersonicVehicleTrajectoryFilePath_J2000 = "filename";
+drag_coeff = 1.0;
+lift_coeff = 1.0;
+surface_area = 1.0;
+mass = 1.0;
 
 
 %% Define analysis functions
 % state vector X = [r, theta, phi, v, gamma, psi]
 % ALL ANGLES IN DEGREES
-function rangeToEarth = calculate_range_to_earth(azimuth, elevation, range, currentLocation)
+function rangeToEarth_km = calculate_range_to_earth(azimuth, elevation, range, currentLocation)
 phi = azimuth;
 th = elevation;
 r = range;
@@ -15,17 +19,35 @@ relCartesian = [r*cosd(th)*cosd(phi);...
                 r*cosd(th)*cosd(phi);...
                 r*sind(th0)];
 
-rangeToEarth = norm(currentLocation + relCartesian);
+rangeToEarth_km = norm(currentLocation + relCartesian);
 end
 
-function Xk = propagate_state(Xk, noise, drag, lift, sigma_deg, C_gamma, C_psi, dt)
 
+function dynamicPressure = calculate_dynamic_pressure(rangeToEarth_km, v)
+rho_0 = 1.225; %kg/m^3
+h_m = (rangeToEarth - 6731.8)*1000;
+hs_m = 6700;
+rho = rho_0.*exp(-h_m/hs_m);
+
+dynamicPressure = rho.*(v.*v)/2;
+end
+
+function accelerations = calculate_flight_accelerations(dynamicPressure, drag_coeff, lift_coeff, surface_area, mass)
+    accelerations = [dynamicPressure.*drag_coeff.*surface_area./mass;
+                     dynamicPressure.*lift_coeff.*surface_area./mass];
+end
+
+
+function Xk = propagate_state(Xk, noise, accelerations, sigma_deg, C_gamma, C_psi, dt)
 r = Xk(1,:);
 th = Xk(2,:);
 phi = Xk(3,:);
 v = Xk(4,:);
 gamma = Xk(5,:);
 psi = Xk(6,:);
+
+drag = accelerations(1,:);
+lift = accelerations(2,:);
 
 
 r_dot = v.*sind(gamma);
